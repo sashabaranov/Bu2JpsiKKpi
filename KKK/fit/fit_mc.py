9839c786071d256198292a129858cbc04a1288d5
Alexander Baranov <a.baranov@cern.ch>
2014-05-15 01:09:22 +0400

    KKK: save mc form via projection, not sPlot

--- KKK/fit/fit_mc.py
+++ KKK/fit/fit_mc.py
@@ -7,7 +7,7 @@ from data import mc_Pythia6, mc_Pythia8, mc_total
 
 
 cuts_Bu += mctrue
-tBu = mc_Pythia6.data
+tBu = mc_Pythia8.data
 
 
 logger.info('DATA chain name is %s ' % (tBu.GetName()))
@@ -29,48 +29,48 @@ for i in prntCuts(cuts_Bu, "  CUTS B+  "):
 
 
 
-from variables import *
+# from variables import *
 
-sel_Bu = SelectorWithVars(
-    variables=selector_variables,
-    selection=cuts_Bu
-)
+# sel_Bu = SelectorWithVars(
+#     variables=selector_variables,
+#     selection=cuts_Bu
+# )
 
-logger.info('Build RooFit dataset for B+ , it could take as long as 3-5 minutes')
+# logger.info('Build RooFit dataset for B+ , it could take as long as 3-5 minutes')
 
-tBu.process(sel_Bu)
+# tBu.process(sel_Bu)
 
-ds_Bu = sel_Bu.dataset()
-ds_Bu.Print('v')
-
-
-logger.info('Make unbinned fit for B+')
-#
-model_Bu.s.setMax(1.2 * len(ds_Bu))
-ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
-
-model_Bu.signal.sigma.release()
-model_Bu.signal.mean.release()
-ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
-
-model_Bu.signal.aR.release()
-model_Bu.signal.aL.release()
-ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
-
-model_Bu.signal.nR.release()
-model_Bu.signal.nL.release()
-
-ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
+# ds_Bu = sel_Bu.dataset()
+# ds_Bu.Print('v')
 
 
-fu.SetXTitle('#Inv.\,mass(J/\psi\,KKK), GeV/c^2')
-fu.SetYTitle('Events / (%d \, MeV/c^{2})' % events_binning)
+# logger.info('Make unbinned fit for B+')
+# #
+# model_Bu.s.setMax(1.2 * len(ds_Bu))
+# ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
 
-fu.Draw()
+# model_Bu.signal.sigma.release()
+# model_Bu.signal.mean.release()
+# ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
+
+# model_Bu.signal.aR.release()
+# model_Bu.signal.aL.release()
+# ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
+
+# model_Bu.signal.nR.release()
+# model_Bu.signal.nL.release()
+
+# ru, fu = model_Bu.fitTo(ds_Bu, draw=True, nbins=nbin_Bu)
 
 
-logger.info('running sPlot')
-model_Bu.sPlot(ds_Bu)
+# fu.SetXTitle('#Inv.\,mass(J/\psi\,KKK), GeV/c^2')
+# fu.SetYTitle('Events / (%d \, MeV/c^{2})' % events_binning)
+
+# fu.Draw()
+
+
+# logger.info('running sPlot')
+# model_Bu.sPlot(ds_Bu)
 
 
 # ===============================================
@@ -78,25 +78,23 @@ model_Bu.sPlot(ds_Bu)
 # ===============================================
 
 
-# hists = [
-#     ("p8_k1", "mass_k1aspi", "SBu_sw"),
-#     ("p8_k3", "mass_k3aspi", "SBu_sw"),
-#     ("p8_k1_cuts", "mass_k1aspi", "SBu_sw && ann_kaon_PI_2 > 0.1"),
-#     ("p8_k3_cuts", "mass_k3aspi", "SBu_sw && ann_kaon_PI_0 > 0.1"),
-#     ("p8_k1_cuts_strong", "mass_k1aspi", "SBu_sw && ann_kaon_PI_2 > 0.1 && ann_kaon0 > 0.3"),
-#     ("p8_k3_cuts_strong", "mass_k3aspi", "SBu_sw && ann_kaon_PI_0 > 0.1 && ann_kaon2 > 0.3"),
-# ]
+hists = [
+    ("p8_k1", "mass_k1aspi", ""),
+    ("p8_k3", "mass_k3aspi", ""),
+    ("p8_k1_cuts", "mass_k1aspi", "ann_kaon_PI[2] > 0.1"),
+    ("p8_k3_cuts", "mass_k3aspi", "ann_kaon_PI[0] > 0.1"),
+]
 
 
-# logger.info('Writing histos')
-# db = shelve.open('$KKpidir/fit/histos.shelve')
+logger.info('Writing histos')
+db = shelve.open('$KKpidir/fit/histos.shelve')
 
-# d = db['KKK']
+d = db['KKK']
 
-# for param in hists:
-#     d['MC'][param[0]] = make_hist(ds_Bu, *param)
+for param in hists:
+    d['MC'][param[0]] = make_hist_mc(tBu, *param)
 
-# db['KKK'] = d
+db['KKK'] = d
 
 
 
@@ -107,21 +105,21 @@ model_Bu.sPlot(ds_Bu)
 
 
 
-# h1, h2 = db['KKK']['MC']['p8_k1_cuts'], db['KKK']['MC']['p8_k3_cuts']
+h1, h2 = db['KKK']['MC']['p8_k1'], db['KKK']['MC']['p8_k3']
 
 
-# title = '#Inv.\,mass(J/\psi\,K^+\,K^-\,K^+) \,\, with \,\, misid, GeV/c^2'
-# h1.SetXTitle(title)
-# h2.SetXTitle(title)
+title = '#Inv.\,mass(J/\psi\,K^+\,K^-\,K^+) \,\, with \,\, misid, GeV/c^2'
+h1.SetXTitle(title)
+h2.SetXTitle(title)
 
-# h1.red()
-# h2.blue()
+h1.red()
+h2.blue()
 
-# h1.Draw()
-# h2.Draw('same')
+h1.Draw()
+h2.Draw('same')
 
 
 
-# db.sync()
-# db.close()
+db.sync()
+db.close()
 
--- KKK/fit/tools.py
+++ KKK/fit/tools.py
@@ -22,3 +22,18 @@ def make_hist(ds, name, variable, cuts):
     h.Scale(1.0/h.Integral())
 
     return h
+
+def make_hist_mc(tBu, name, variable, cuts):
+    h = ROOT.TH1F(name, '', 30, 5.16, 5.45)
+    h.Sumw2()
+
+    tBu.Project(h.GetName(), variable, cuts)
+
+    # for j in xrange(0, h.GetNbinsX()):
+    #     if h.GetBinContent(j) < 0:
+    #         h.SetBinContent(j, 0)
+
+    h.Scale(1.0/h.Integral())
+
+    return h
+

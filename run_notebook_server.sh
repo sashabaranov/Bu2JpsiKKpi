e5428a19db3fd016227e09425be76bea0168eafb
Sasha Baranov <a.baranov@cern.ch>
2014-06-17 13:09:43 +0200

    Notebook server script

---
+++ run_notebook_server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+PORT=8001
+
+ROOT_VERSION=67
+check() {
+    which runpy > /dev/null
+    if [ $? -eq 1 ] ; then
+        echo "Did you run `source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v7r10p1/InstallArea/scripts/LbLogin.sh`?"
+        exit
+    fi
+}
+
+check
+hostname -f
+uptime
+free -m
+
+export ENV4MU=/afs/cern.ch/work/a/albarano/public/4mu_env
+
+source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v7r9p1/InstallArea/scripts/SetupProject.sh Bender v24r0
+source $ENV4MU/bin/activate
+
+# Dirty hack to make things work
+export PYTHONPATH=$ENV4MU/lib64/python2.7/site-packages/:$PYTHONPATH
+
+ipython notebook --ip='*' --pylab inline --no-browser --port=$PORT

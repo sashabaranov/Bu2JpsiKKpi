fc175ec48d359c92140d5592e8e437fbeeef0b8e
Sasha Baranov <a.baranov@cern.ch>
2014-10-09 19:07:26 +0200

    Updated server script for Bender v25r0

--- run_notebook_server.sh
+++ run_notebook_server.sh
@@ -17,7 +17,7 @@ free -m
 
 export ENV4MU=/afs/cern.ch/work/a/albarano/public/4mu_env
 
-source SetupProject.sh Bender v24r2
+source SetupProject.sh Bender v25r0
 source $ENV4MU/bin/activate
 
 # Dirty hack to make things work
